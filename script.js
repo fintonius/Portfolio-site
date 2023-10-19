557be7cd104ba1ee1e711e1ac1b5ea4eff697c84
fintonius <fintonius.mcginty@gmail.com>
2023-10-19 09:43:53 +0100

    remove default styling from button and add a click  event listener

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// use local storage to save the dark-mode state user has chosen
 let darkMode = localStorage.getItem('darkMode');
+
+const darkModeButton = document.querySelector('#dark-mode');
+
+darkModeButton.addEventListener('click', () => {
+    console.log('click!');
+})
